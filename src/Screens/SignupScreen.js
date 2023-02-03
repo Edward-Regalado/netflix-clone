76e4e061680dae5cbe089623828e40bd72ba8e68
Edward-Regalado <tregalado30@gmail.com>
2023-02-03 04:46:10 +0000

    started signup/login screens

--- src/Screens/SignupScreen.js
+++ src/Screens/SignupScreen.js
@@ -2,9 +2,27 @@ import React from 'react';
 import './SignupScreen.css';
 
 function SignupScreen() {
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className='signupScreen'>
-      SignInScreen
+    <div className='signUpScreen'>
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder="Email" type="email"/>
+        <input placeholder="Password" type="password"/>
+        <button type='submit' onClick={signIn}>Sign In</button>
+        <h4>
+          <span className='signUpScreen-gray'>New to Netflix?</span>
+          <span className='signUpScreen-link' onClick={register}>Sign up now.</span>
+        </h4>
+      </form>
     </div>
   );
 }
